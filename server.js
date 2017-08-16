5de6585fc9656d6b07d1cc24c0aeefc86447bddb
sangeethathirumoorthy <31065763+sangeethathirumoorthy@users.noreply.github.com>
2017-08-16 17:51:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/-article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('Article one was requested and server here');
 });
 
-app.get('/-article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article two was requested and server here');
 });
 
-app.get('/-article-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article three was requested and server here');
 });
 
