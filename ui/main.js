c31787a98f7a4873657f6306fb26231cc11d29a8
sangeethathirumoorthy <31065763+sangeethathirumoorthy@users.noreply.github.com>
2017-08-18 14:22:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //var changes
 var element=document.getElementById("main-text");
 element.innerHTML="NEW VALUE"
+
+// move the image
+var img=document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
