cbbd3b13fb54d2f388ed35b9d84c4bcadd215fae
sangeethathirumoorthy <31065763+sangeethathirumoorthy@users.noreply.github.com>
2017-08-18 12:42:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,10 +83,10 @@ var htmlTemplate=`
             <a href="/">home</a>
         </div>
         <hr/>
+        ${date}
         <h3>
             ${heading}
         </h3>
-        ${date}
         <div>
             ${title}
         </div>
@@ -113,6 +113,13 @@ app.get('/:articleName', function (req, res) {
  res.send(createTemplate(articles[articleName]));
 });
 
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
